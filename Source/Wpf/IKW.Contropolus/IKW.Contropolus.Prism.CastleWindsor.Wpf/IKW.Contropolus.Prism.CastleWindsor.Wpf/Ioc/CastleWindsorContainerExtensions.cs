a35a8735cee0129b7d831287fff5372a3fb3d571
Tavi Truman <TaviTruman@users.noreply.github.com>
2019-05-28 14:34:24 -0700

    ikw Castle Windsor integration updates

---
+++ Source/Wpf/IKW.Contropolus/IKW.Contropolus.Prism.CastleWindsor.Wpf/IKW.Contropolus.Prism.CastleWindsor.Wpf/Ioc/CastleWindsorContainerExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Castle.Windsor;
+using Prism.Ioc;
+
+namespace IKW.Contropolus.Prism.CastleWindsor.Wpf.Ioc
+{
+    public class CastleWindsorContainerExtensions : IContainerExtension<IWindsorContainer>
+    {
+        public IWindsorContainer Instance { get; }
+
+        public void FinalizeExtension()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRegistered(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsRegistered(Type type, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContainerRegistry Register(Type from, Type to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContainerRegistry Register(Type from, Type to, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContainerRegistry RegisterInstance(Type type, object instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContainerRegistry RegisterInstance(Type type, object instance, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContainerRegistry RegisterSingleton(Type from, Type to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContainerRegistry RegisterSingleton(Type from, Type to, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type type, params (Type Type, object Instance)[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type type, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type type, string name, params (Type Type, object Instance)[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
