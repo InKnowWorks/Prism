1a7415e53933b15ef818925d7bd62068115b02c3
Tavi Truman <tavi@inknowworks.com>
2020-02-27 10:15:32 -0800

    Prep to add additonal Windsor Container Extenstions

---
+++ Source/Wpf/IKW.Contropolus/IKW.Contropolus.Prism.CastleWindsor.WPF/Ioc/IWindsorContainerExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace IKW.Contropolus.Prism.CastleWindsor.WPF.Ioc
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IWindsorContainerExtensions
+    {
+        
+    }
+}
